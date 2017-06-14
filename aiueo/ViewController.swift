7cf8f926146d878ed10f23f84d14a12d19ea56ef
兼子友花 <0416yuuka@gmail.com>
2017-06-14 19:25:00 +0900

    Initial Commit

---
+++ aiueo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aiueo
+//
+//  Created by 兼子友花 on 2017/06/14.
+//  Copyright © 2017年 兼子友花. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ aiueoTests/aiueoTests.swift
@@ -0,0 +1,36 @@
+//
+//  aiueoTests.swift
+//  aiueoTests
+//
+//  Created by 兼子友花 on 2017/06/14.
+//  Copyright © 2017年 兼子友花. All rights reserved.
+//
+
+import XCTest
+@testable import aiueo
+
+class aiueoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
