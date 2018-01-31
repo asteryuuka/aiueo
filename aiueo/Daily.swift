91fcab676e319598e5e0ef8b3b79ecc470ff70bc
兼子友花 <0416yuuka@gmail.com>
2018-01-31 20:15:17 +0900

    前の日のScheduleを保存して呼び出せるようにする

---
+++ aiueo/Daily.swift
@@ -0,0 +1,15 @@
+//
+//  Daily.swift
+//  Floaty
+//
+//  Created by 兼子友花 on 2018/01/31.
+//
+
+import RealmSwift
+
+
+class Daily: Object{
+    dynamic var schedule: Int = 0 
+    dynamic var date: Date
+    //schedule.swiftで記憶したスタンプごとの情報を日にちごとにまとめる
+}
---
+++ aiueo/Schedule.swift
@@ -0,0 +1,17 @@
+//
+//  Schedule.swift
+//  Floaty
+//
+//  Created by 兼子友花 on 2018/01/31.
+//
+
+
+import RealmSwift
+
+class Schedule: Object{
+    dynamic var startIndex: Int = 0
+    dynamic var endIndex: Int = 0
+    dynamic var stamp: stamp?
+    
+    //なんのスタンプがどこに配置されてたかを記憶する
+}
